bc8e6fbb252ad7ca0291927bc888e9170b10843a
vshaldb <vissuvishal1425.com>
2024-06-19 00:22:20 +0530

    Contact details

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I’m actively looking for new opportunities or projects in DevOps, Drop a mail if you want to
+        connect &amp; discuss on how <del>to make this world better</del> can we collaborate.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
