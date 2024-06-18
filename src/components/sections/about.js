0b0efb4607a7e05ceef4fef5e5cd1d2fef84ffdd
vshaldb <vissuvishal1425.com>
2024-06-18 23:46:38 +0530

    about.js updated

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,14 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'Microsoft Azure',
+    'Docker',
+    'Kubernetes',
+    'Terraform',
+    'Azure DevOps (ADO)',
+    'Packer',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +142,24 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Vishal Dangudubiyyapu, and I enjoy building &amp; deploying large
+              applications on the cloud. My interest in the cloud started back in university when I
+              was enrolled in AWS to begin with cloud computing Later, I completed Application
+              Development and Deployment Architecture course, in which I learned about shipping
+              applications on Docker.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              In the final year of my university, I got an opportunity to work in Synopsys Cloud
+              team as a DevOps engineer, and I’ve had the privilege to work on multiple cloud
+              platforms, including AWS &amp; MS Azure. Here, building microservices on docker and
+              deploying them to AKS using Helm Charts was my major responsibility.{' '}
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I also had the privilege to work on IaC tools like Terraform Provisioning
+              Infrastructure on MS Azure, leveraging Packer to build generalized images for VMSS,
+              etc.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
